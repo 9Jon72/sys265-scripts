4772e114d962b7ec7dc37b633ad390d7831fb5f1
jon <jonathan.hanyok@mymail.champlain.edu>
2019-02-25 19:57:19 -0500

    updating script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,3 +4,10 @@
 #Adds a publuc key to that users authorized keys file
 #Removes roots ability to ssh in
 echo "this is cool af"
+sudo useradd sys265
+sudo mkdir /home/sys265/.ssh
+sudo cp linux/public-keys/sys265.pub /home/sys265/.ssh/authorized_keys
+sudo chmod 700 /home/sys265/.ssh
+sudo chmod 600 /home/sys265/.ssh/authorized_keys
+sudo chown -R sys265:sys265 /home/sys265/.ssh
+
